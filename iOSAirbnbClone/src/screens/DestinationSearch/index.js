a6821be27f18ed0aaf6cd953d9ba03a37ad0dc65
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-27 18:35:44 -0400

    Displays: SearchResults component on simulator, with vector location pin icons and styles, rendered correctly, looking good

--- iOSAirbnbClone/src/screens/DestinationSearch/index.js
+++ iOSAirbnbClone/src/screens/DestinationSearch/index.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {View, TextInput, Text, FlatList} from 'react-native';
 import styles from './styles';
 import searchResults from '../../../assets/data/search';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const DestinationSearchScreen = () => {
   const [inputText, setInputText] = useState('');
@@ -19,7 +20,14 @@ const DestinationSearchScreen = () => {
       {/* List of destinations */}
       <FlatList
         data={searchResults}
-        renderItem={({item}) => <Text>{item.description}</Text>}
+        renderItem={({item}) => (
+          <View style={styles.row}>
+            <View style={styles.iconContainer}>
+              <Entypo name={'location-pin'} size={24} />
+            </View>
+            <Text style={styles.locationText}>{item.description}</Text>
+          </View>
+        )}
       />
     </View>
   );
