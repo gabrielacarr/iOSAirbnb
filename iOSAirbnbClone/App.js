57ced222459a422fbe93eb100cba6eb734da1ee1
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-27 16:40:22 -0400

    Displays: Post component with file structure and styles, looking good

--- iOSAirbnbClone/App.js
+++ iOSAirbnbClone/App.js
@@ -9,13 +9,14 @@ import Entype from 'react-native-vector-icons/Entypo';
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import HomeScreen from './src/screens/Home';
-
+import Post from './src/components/Post';
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <HomeScreen />
+        {/* <HomeScreen /> */}
+        <Post />
       </SafeAreaView>
     </>
   );
---
+++ iOSAirbnbClone/src/components/Post/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+const Post = () => {
+  return <Text>Hello, I am your Post component on your screen</Text>;
+};
+
+export default Post;
