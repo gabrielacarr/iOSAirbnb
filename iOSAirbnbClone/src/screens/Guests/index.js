0383dc2f6a18cd7f07a7564dfc509ee626a37c44
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-27 19:38:59 -0400

    Implmenation and Display:GuestScreens with useState to render calculations for the adult buttons with styles, looking good

--- iOSAirbnbClone/src/screens/Guests/index.js
+++ iOSAirbnbClone/src/screens/Guests/index.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Pressable} from 'react-native';
 import styles from './styles';
 
 const GuestScreen = () => {
+  const [adults, setAdults] = useState(0);
+
   return (
     <View>
       {/* Row One Adults */}
-      {/* Titles */}
-      {/* Buttons with given value  */}
-      {/* - */}
-      {/* + */}
+      <View style={styles.row}>
+        {/* Titles */}
+        <View>
+          <Text style={{fontWeight: 'bold'}}>Adults</Text>
+          <Text style={{color: 'grey'}}>Ages 13 or above</Text>
+        </View>
+
+        {/* Buttons with given value  */}
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            fontSize: 15,
+          }}>
+          {/* - */}
+          <Pressable
+            onPress={() => setAdults(Math.max(0, adults - 1))}
+            style={styles.button}>
+            <Text style={{color: 'grey'}}>-</Text>
+          </Pressable>
+
+          <Text style={{marginHorizontal: 20}}>{adults}</Text>
+
+          {/* + */}
+          <Pressable
+            onPress={() => setAdults(adults + 1)}
+            style={styles.button}>
+            <Text style={{color: 'grey'}}>+</Text>
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 };
