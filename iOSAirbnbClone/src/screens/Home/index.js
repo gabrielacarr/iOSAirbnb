141d48aefda86e7c284540fe4d36e109c4bf867f
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-27 16:13:25 -0400

    Displays: Homescreen with search bar button and console warn when pressed, looking good

--- iOSAirbnbClone/src/screens/Home/index.js
+++ iOSAirbnbClone/src/screens/Home/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {View, Text, ImageBackground, Pressable} from 'react-native';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 import styles from './styles';
 
 const HomeScreen = props => {
   return (
     <View>
       {/* Search Bar Component */}
+      <Pressable
+        style={styles.searchButton}
+        onPress={() => console.warn('Search btn pressed')}>
+        <Fontisto name="search" size={25} color={'#f15454'} />
+        <Text style={styles.searchButtonText}>Where are you traveling?</Text>
+      </Pressable>
 
       {/* {Image Background} */}
       <ImageBackground
--- iOSAirbnbClone/src/screens/Home/styles.js
+++ iOSAirbnbClone/src/screens/Home/styles.js
@@ -1,6 +1,23 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 
 const styles = StyleSheet.create({
+  searchButton: {
+    backgroundColor: '#fff',
+    height: 60,
+    width: Dimensions.get('screen').width - 20,
+    borderRadius: 30,
+    marginHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 50,
+    zIndex: 100,
+  },
+  searchButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
   image: {
     width: '100%',
     height: 500,
