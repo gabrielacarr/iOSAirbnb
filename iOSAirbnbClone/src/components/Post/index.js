c135c42e8f52881efdeff71b3c0f73cf857292de
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-27 16:44:04 -0400

    TODO: bed, bath + type description, old/new price, total price

--- iOSAirbnbClone/src/components/Post/index.js
+++ iOSAirbnbClone/src/components/Post/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import styles from './styles';
+
 const Post = () => {
-  return <Text>Hello, I am your Post component on your screen</Text>;
+  return (
+    <View style={styles.container}>
+      {/* Bed and Bedroom */}
+
+      {/* Type description */}
+
+      {/* Old and New price */}
+
+      {/* Total price */}
+    </View>
+  );
 };
 
 export default Post;
