63576ebe0c623b582c519303f18ba6c92e6d2c6d
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-27 16:54:12 -0400

    Displays: Post component with dummy data, looking good

--- iOSAirbnbClone/src/components/Post/index.js
+++ iOSAirbnbClone/src/components/Post/index.js
@@ -14,12 +14,25 @@ const Post = () => {
         }}
       />
       {/* Bed and Bedroom */}
+      <Text style={styles.bedrooms}>
+        2 guests · 1 bedroom · 1 bed · 1 bath{' '}
+      </Text>
 
       {/* Type description */}
+      <Text style={styles.description}>
+        An entire spacious, light-filled, cozy tiny home all to yourselves with
+        separate parking on our beautiful 10 acre property, Dummy text for
+        applications.
+      </Text>
 
       {/* Old and New price */}
+      <Text style={styles.prices}>
+        <Text style={styles.oldPrice}>$152</Text>
+        <Text style={styles.newPrice}>$144</Text>
+      </Text>
 
       {/* Total price */}
+      <Text style={styles.totalPrice}>$176</Text>
     </View>
   );
 };
