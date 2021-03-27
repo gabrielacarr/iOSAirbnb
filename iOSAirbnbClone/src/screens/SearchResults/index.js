ef18a1dc0722af6105bc593d318d384cf88fd2d2
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-03-27 17:53:00 -0400

    Displays: SearchResultScreen component on simulator rendered correctly using FlatList, missing styles, looking goood

--- iOSAirbnbClone/src/screens/SearchResults/index.js
+++ iOSAirbnbClone/src/screens/SearchResults/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, FlatList} from 'react-native';
+import Post from '../../components/Post';
+import feed from '../../../assets/data/feed';
 
 const SearchResultScreen = props => {
   return (
     <View>
-      <Text>Can you see me, I am your search results screen component </Text>
+      <FlatList data={feed} renderItem={({item}) => <Post post={item} />} />
     </View>
   );
 };
